6eab8b6be58e2d5710ce594c134965e5c036fbda
devevo <vlp74199@psoxs.com>
2017-03-19 02:21:09 +0300

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -70,38 +70,14 @@ function create_config( )
 		"tools",
 		"fun",
 		"info"
+		"reply"
 	},
-    sudo_users = {157059515},
+    sudo_users = {246281248,0},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v4.0
-An advanced administration bot based on https://valtman.name/telegram-cli
-
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@Rixel ➣ Developer 《
-》@Exacute ➣ Developer《
-》@To0fan ➣ Developer《
-》@CiveY ➣ Developer
-》@ArmanDev ➣ Manager《
-》@MrPars ➣ Manager《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@K_a_I_i_I_i_n_u_x
-》@Nero_Dev
-》And Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
+    info_text = [[》
+		https://t.me/dev_evo
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
